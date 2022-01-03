40fb7ddd69470c279877569792db1a4dd7b3f255
Ian S <iansilber32@gmail.com>
2022-01-03 15:52:06 -0500

    fixes master key and such

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,16 +1,16 @@
 
-# User.create!([
-#   {name: "Ian", email: "Ian@test.com", password_digest: "$2a$12$5ADbgxezP/GD9TRsuHTs3.p3LoKMSoL11pCi/WRbfkP/Q87Hfee5O"},
-# ])
-# Position.create!([])
-# Watchlist.create!([
-#   { name: "Currently watching", user_id: 1 }
-# ])
-# Asset.create!([
-#   { symbol: "BTC", watchlist_id: 1 },
-#   { symbol: "ETH", watchlist_id: 1 },
-#   { symbol: "SOL", watchlist_id: 1 }
-# ])
+User.create!([
+  {name: "Ian", email: "Ian@test.com", password_digest: "$2a$12$5ADbgxezP/GD9TRsuHTs3.p3LoKMSoL11pCi/WRbfkP/Q87Hfee5O"},
+])
+Position.create!([])
+Watchlist.create!([
+  { name: "Currently watching", user_id: 1 }
+])
+Asset.create!([
+  { symbol: "BTC", watchlist_id: 1 },
+  { symbol: "ETH", watchlist_id: 1 },
+  { symbol: "SOL", watchlist_id: 1 }
+])
 TopCoin.create!([
   { rank: "", name: "", symbol: "", percent_change_24h: 0, percent_change_7d: 0, percent_change_30d: 0 },
   { rank: "", name: "", symbol: "", percent_change_24h: 0, percent_change_7d: 0, percent_change_30d: 0 },
