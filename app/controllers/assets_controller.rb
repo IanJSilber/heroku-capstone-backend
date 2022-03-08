56fc9ca67d899942dcc40c920285879acbaf02a5
Ian S <iansilber32@gmail.com>
2022-03-08 16:07:24 -0500

    reverts changes in the assets controller

--- app/controllers/assets_controller.rb
+++ app/controllers/assets_controller.rb
@@ -5,17 +5,19 @@ class AssetsController < ApplicationController
   def index
     assets = Asset.where(watchlist_id: params[:watchlist_id]) # select the assets based on watchlist
 
-    assets.each do |asset|
-      symbol = asset.symbol # get the symbol
+    i = 0
+    while i < assets.length
+      symbol = assets[i].symbol # get the symbol
       request = HTTP.get("https://pro-api.coinmarketcap.com/v1/cryptocurrency/quotes/latest?CMC_PRO_API_KEY=#{$cmc_api_keys.sample}&symbol=#{symbol}")
       request = request.parse(:json) # make the request using the symbol
-      asset.name = request["data"][symbol]["name"]
-      asset.price = '%.2f' % request["data"][symbol]["quote"]["USD"]["price"]
-      asset.percent_change_1h = '%.2f' % request["data"][symbol]["quote"]["USD"]["percent_change_1h"]
-      asset.percent_change_24h = '%.2f' % request["data"][symbol]["quote"]["USD"]["percent_change_24h"]
-      asset.percent_change_7d = '%.2f' % request["data"][symbol]["quote"]["USD"]["percent_change_7d"]
-      asset.percent_change_30d = '%.2f' % request["data"][symbol]["quote"]["USD"]["percent_change_30d"]
+      assets[i].name = request["data"][symbol]["name"]
+      assets[i].price = '%.2f' % request["data"][symbol]["quote"]["USD"]["price"]
+      assets[i].percent_change_1h = '%.2f' % request["data"][symbol]["quote"]["USD"]["percent_change_1h"]
+      assets[i].percent_change_24h = '%.2f' % request["data"][symbol]["quote"]["USD"]["percent_change_24h"]
+      assets[i].percent_change_7d = '%.2f' % request["data"][symbol]["quote"]["USD"]["percent_change_7d"]
+      assets[i].percent_change_30d = '%.2f' % request["data"][symbol]["quote"]["USD"]["percent_change_30d"]
       # get the required data from the request
+      i += 1
     end
     render json: assets
   end
