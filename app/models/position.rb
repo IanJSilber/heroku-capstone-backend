7eb323b53c4dd9b19b820db08410cc0d425f9e12
ijs32 <iansilber32@gmail.com>
2022-04-10 23:58:10 -0400

    testing for the position model

--- app/models/position.rb
+++ app/models/position.rb
@@ -2,12 +2,12 @@ class Position < ApplicationRecord
   belongs_to :user
   validates :symbol, :amount, :user_id, presence: true # make sure symbol and amount is present
   validates :symbol, uniqueness: true # make sure the symbol is unique; a user doesnt already have a BTC position, etc.
-  before_validation :uppercase_symbol # make sure the symbol is capitalized
+  # before_validation :uppercase_symbol # make sure the symbol is capitalized
 
   # Uppercase symbol before creating a position
-  def uppercase_symbol
-    symbol.upcase!
-  end
+  # def uppercase_symbol
+  #   symbol.upcase!
+  # end
 
   # get total value of a position by multiplying current price * amount
   def position_value
@@ -26,11 +26,11 @@ class Position < ApplicationRecord
 
   #get profit and loss in dollars over the past 7 days
   def pnl_7_days
-    (purchase_price.to_f * percent_change_7d.to_f).round(2)  
+    (purchase_price.to_f * percent_change_7d.to_f).round(2)
   end
 
   #get profit and loss in dollars over the past 30 days
   def pnl_30_days
-    (purchase_price.to_f * percent_change_30d.to_f).round(2)  
+    (purchase_price.to_f * percent_change_30d.to_f).round(2)
   end
 end
--- spec/models/position_spec.rb
+++ spec/models/position_spec.rb
@@ -1,5 +1,37 @@
 require 'rails_helper'
 
 RSpec.describe Position, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:user) {
+    User.new(:email => "ian@example.com", :password => "password")
+  }
+  subject { described_class.new(
+    symbol: "BTC",
+    amount: 2000,
+    user_id: 1)
+  }
+
+  it "is valid with valid attributes" do
+    expect(subject).to be_valid
+  end
+
+  it "is not valid without user_id" do
+    subject.user_id = nil
+    expect(subject).to_not be_valid
+  end
+
+  it "is not valid without symbol" do
+    subject.symbol = nil
+    expect(subject).to_not be_valid
+  end
+
+  it "is not valid without amount" do
+    subject.amount = nil
+    expect(subject).to_not be_valid
+  end
+
+  # Testing associations
+  describe "Associations" do
+    it { should belong_to(:user).without_validating_presence }
+  end
+
 end
